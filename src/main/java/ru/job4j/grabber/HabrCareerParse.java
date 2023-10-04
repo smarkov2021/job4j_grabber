0c215af5799613f8fee8387258c96142ec006089
smarkov <markovsv777@mail.ru>
2023-10-04 18:31:47 +0300

    HabrCareerParse

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,7 +7,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class HabrCareerParse {
 
@@ -15,18 +18,35 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    private static final int NUMB_OF_PAGES = 5;
+
+    private static List<String> generatePagesLinks(int number) {
+        List<String> pages = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            pages.add(PAGE_LINK + "?page=" + i);
+        }
+        return pages;
+    }
+
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element dateTime = row.select(".vacancy-card__date").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String date = dateTime.child(0).attr("datetime");
-            System.out.printf("%s %s %s %n", vacancyName, link, date);
+        generatePagesLinks(NUMB_OF_PAGES).stream().forEach(currentLink -> {
+            Connection connection = Jsoup.connect(currentLink);
+            Document document = null;
+            try {
+            document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element dateTime = row.select(".vacancy-card__date").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String date = dateTime.child(0).attr("datetime");
+                System.out.printf("%s %s %s %n", vacancyName, link, date);
+                });
+                    } catch (IOException e) {
+                    throw new RuntimeException(e);
+            }
         });
     }
 }
