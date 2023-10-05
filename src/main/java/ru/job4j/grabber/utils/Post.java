b6220edc05c943753f21e987428169e95822d8b8
smarkov <markovsv777@mail.ru>
2023-10-05 16:39:37 +0300

    Post

---
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -0,0 +1,38 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", title='" + title + '\''
+                + ", link='" + link + '\'' + ", description='"
+                + description + '\'' + ", created=" + created + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && title.equals(post.title) && description.equals(post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description);
+    }
+}
